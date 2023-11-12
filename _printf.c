1a618233d1f91afdf47735c8a638b1a546782002
AbdulrazaqAS <abdulrazaq2621@gmail.com>
2023-11-12 08:59:22 +0000

    printf

---
+++ _printf.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+int _printf(char *format, ...)
+{
+	va_list args;
+	int written_chars = 0;
+
+	if (format == NULL)
+	{
+		return (-1);
+	}
+
+	return (written_chars);
+}
---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef _PRINTF
+#define _PRINTF
+
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+int _printf(char *format, ...);
+
+#endif /* _PRINTF */
